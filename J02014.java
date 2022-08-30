e0e199e7df1af52a9accf6c55b0726381de85b15
Đỗ Trần Chí <104625735+SuperChis@users.noreply.github.com>
2022-08-30 18:38:55 +0700

    ĐIỂM CÂN BẰNG

---
+++ J02014.java
@@ -0,0 +1,28 @@
+
+package CODE_PTIT;
+
+import java.util.Scanner;
+
+public class J02014 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int s = 0, p = -1, x = 0;
+            int a[] = new int[n + 1];
+            for (int i = 1; i <= n; i++) {
+                a[i] = sc.nextInt();
+                s += a[i];
+            }
+            for (int i = 1; i <= n; i++) {
+                if (x * 2 + a[i] == s) {
+                    p = i;
+                    break;
+                }
+                x += a[i];
+            }
+            System.out.println(p);
+        }
+    }
+}
