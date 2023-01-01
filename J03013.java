6c96ae55ecbb269ac3b0940f3a3420d470d5941b
Đỗ Trần Chí <104625735+SuperChis@users.noreply.github.com>
2023-01-01 22:50:33 +0700

    HIỆU SỐ NGUYÊN LỚN - 1

---
+++ J03013.java
@@ -0,0 +1,17 @@
+package CODE_PTIT;
+
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class J03013 {
+    
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int t = Integer.parseInt(inp.nextLine());     
+        while(t-- > 0){
+            BigInteger a = inp.nextBigInteger();
+            BigInteger b = inp.nextBigInteger();
+            System.out.println(a.add(b));
+        }
+    }
+}
